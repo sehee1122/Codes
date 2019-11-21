27d9a33f94751bb7cb249a28d1ff8b6250d38f48
sehee1122 <55127603+sehee1122@users.noreply.github.com>
2019-11-21 15:25:12 +0900

    Practice12

--- 12_string/main.c
+++ 12_string/main.c
@@ -40,7 +40,28 @@ int main(int argc, char *argv[]) {
 	
 	printf("Length of String \"%s\" is %i\n\n", str2, strlen(str2));
 	
+	// Practice 12-5
 	
+	int j;
+	char input[100];
+	FILE *fp1;
+	FILE *fp2;
+	
+	fp1 = fopen("sample.txt", "w");
+	for(j = 0; j < 3; j++) {
+		printf("input a word : ");
+		scanf("%s", input);
+		fprintf(fp1, "%s\n", input);
+	}
+	fclose(fp1);
+	printf("\n");
+	
+	fp2 = fopen("sample.txt", "r");
+	for(j = 0; j < 3; j++) {
+		fscanf(fp2, "%s", input);
+		printf("%s\n", input);
+	}
+	fclose(fp2);
 	
 	return 0;
 }
